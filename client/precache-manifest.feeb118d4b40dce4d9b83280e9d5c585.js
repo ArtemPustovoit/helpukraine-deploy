0d411b0e3399257bf9c78037de500d48376c8263
Artem Pustovoit <ampustovoit@gmail.com>
2022-05-15 21:40:00 +0200

    third commit

---
+++ client/precache-manifest.feeb118d4b40dce4d9b83280e9d5c585.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6aa367888ac870ca27e9f64d2571d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c5707ec459815e8d48d",
+    "url": "/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "43dd9edfa254b72b5544",
+    "url": "/static/css/main.f29864df.chunk.css"
+  },
+  {
+    "revision": "2c5707ec459815e8d48d",
+    "url": "/static/js/2.899fa762.chunk.js"
+  },
+  {
+    "revision": "b303cf1a9e78c3d573683305f459fd10",
+    "url": "/static/js/2.899fa762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43dd9edfa254b72b5544",
+    "url": "/static/js/main.ad654ec7.chunk.js"
+  },
+  {
+    "revision": "20bfd819001ceebb5fa9",
+    "url": "/static/js/runtime-main.13596319.js"
+  },
+  {
+    "revision": "cd98e2c39b50532ef09a3344e820499f",
+    "url": "/static/media/cancun.cd98e2c3.jpg"
+  },
+  {
+    "revision": "df994899b2925871e24126812c416c24",
+    "url": "/static/media/dubai.df994899.jpg"
+  },
+  {
+    "revision": "841651c32f46a04c181eb090a32bd103",
+    "url": "/static/media/london.841651c3.jpg"
+  },
+  {
+    "revision": "73d8ec963252e42850a528642374f476",
+    "url": "/static/media/los-angeles.73d8ec96.jpg"
+  },
+  {
+    "revision": "c21df1475cabff80a179b3c0cd49dd67",
+    "url": "/static/media/map-background.c21df147.jpg"
+  },
+  {
+    "revision": "23aadf8146a0240ddc18f846d8c64843",
+    "url": "/static/media/san-fransisco.23aadf81.jpg"
+  },
+  {
+    "revision": "3c573d340e505c3f2f5b60c34b20aacc",
+    "url": "/static/media/toronto.3c573d34.jpg"
+  }
+]);
